a815ba9679ec2db223689b98a26eaa2a5e22d5ec
GlermS <guigoventapane@gmail.com>
2021-02-26 09:20:13 +0100

    Fonction pour la prochaine question, pour verifier la réponse, pour reecrir la question

--- app/src/main/java/musique/quiz.java
+++ app/src/main/java/musique/quiz.java
@@ -16,6 +16,7 @@ import controleur.gameActivity;
 
 public class quiz extends AppCompatActivity {
     public static int juste1;
+    public static int MontrerReponse;
     private TextView question11;
     private Button reponse11;
     private Button reponse21;
@@ -25,48 +26,28 @@ public class quiz extends AppCompatActivity {
     private TextView NbPoint1;
 
     public static int temp1;
+    //public static int MontrerReponse;
+
+    //Question | Op1 | Op2 | Op3 | Op4 | Bonne Réponse
+    String[][] questionsMUSIQUE = {
+            /*0*/{"La chanteuse londonienne Miss Adkins est mieux connue sous quel nom?", "Adele", "Élisabeth", "Jessica Petenon", "Jessie J", "Adele"},
+            /*1*/{"Quel chanteuse a chanté la chanson «Like a Virgin»?", "Madonna", "Miley Cyrus", "Cyndi Lauper", "Cheryl Fernandez-Versini", "Madonna"},
+            /*2*/{"Qui est devenu célèbre en 2008 avec la sortie du single «I Kissed a Girl»?", "Madonna", "Katy Perry", "Selena Gomez", "Justin Bieber", "Katy Perry"},
+            /*3*/{"Combien de cordes un violon a-t-il habituellement?", "Cinq", "Quatre", "Six", "Sept", "Quatre"},
+            /*4*/{"Dans quelle ville américaine Elvis a-t-il été découvert mort en 1977?", "Memphis", "Los Angeles", "Las Vegas", "Macaé", "Memphis"}
+            /*5*///{"", "", "", "", "", ""},
+            /*6*///{"", "", "", "", "", ""},
+            /*7*///{"", "", "", "", "", ""},
+            /*8*///{"", "", "", "", "", ""},
 
 
+            /*n*///{"", "", "", "", "", ""},
+    };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Fonction Pas Encore Finie
-        /*public void ChangerColeurs(String reponse; String bonneReponse) {
-            if(reponse11.text == bonneReponse){
-            reponse11.setBackgroundColor(Color.parseColor("#FF0000"));
-            points +1;
-            }else  reponse11.setBackgroundColor(Color.parseColor("#autre"));
-
-            reponse21.setBackgroundColor(Color.parseColor("#FF0000"));
-            reponse31.setBackgroundColor(Color.parseColor("#FF0000"));
-            reponse41.setBackgroundColor(Color.parseColor("#FF0000"));
-
-            suite.setEnabled(true);
-            reponse11.setEnabled(false);
-            reponse21.setEnabled(false);
-            reponse31.setEnabled(false);
-            reponse41.setEnabled(false);
-
-        }
-*/
-
-        //Question | Op1 | Op2 | Op3 | Op4 | Bonne Réponse
-        String[][] questionsMUSIQUE = {
-        /*0*/{"La chanteuse londonienne Miss Adkins est mieux connue sous quel nom?", "Adele", "Élisabeth", "Jessica Petenon", "Jessie J", "Adele"},
-        /*1*/{"Quel chanteuse a chanté la chanson «Like a Virgin»?", "Madonna", "Miley Cyrus", "Cyndi Lauper", "Cheryl Fernandez-Versini", "Madonna"},
-        /*2*/{"Qui est devenu célèbre en 2008 avec la sortie du single I Kissed a Girl?", "Madonna", "Katy Perry", "Selena Gomez", "Justin Bieber", "Katy Perry"},
-        /*3*/{"Combien de cordes un violon a-t-il habituellement?", "Cinq", "Quatre", "Six", "Sept", "Quatre"},
-        /*4*/{"Dans quelle ville américaine Elvis a-t-il été découvert mort en 1977?", "Memphis", "Los Angeles", "Las Vegas", "Macaé", "Memphis"}
-        /*5*///{"", "", "", "", "", ""},
-        /*6*///{"", "", "", "", "", ""},
-        /*7*///{"", "", "", "", "", ""},
-        /*8*///{"", "", "", "", "", ""},
-
-
-        /*n*///{"", "", "", "", "", ""},
-        };
-
+        //Creation page Quiz avec questions
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         question11 = (TextView) findViewById(R.id.activity_question11_txt);
@@ -77,6 +58,7 @@ public class quiz extends AppCompatActivity {
         suite = (Button) findViewById(R.id.activity_suite1_btn);
         NbPoint1 = (TextView) findViewById(R.id.activity_NbPoint1_txt);
 
+        //Fonction pour choisir question aleatoirement
         /*public int NombreAleatiore() {
             return (int) ((Math.random() * (0 - 4)) + 0);
             //if not dans ll'array historique
@@ -85,7 +67,8 @@ public class quiz extends AppCompatActivity {
         int questionChoisi = 2;
         //questionChoisi = (int) ((Math.random() * (1 - 4)) + 1); //ça ne marche pas
 
-      //  public void ReecrierQuestions() { /*Fonction pour changer de question chaque fois*/
+        //Fonction pour changer de question chaque fois que le button Next est cliqué
+      //  public void ReecrierQuestions() {
             question11.setText(questionsMUSIQUE[questionChoisi][0]);
             reponse11.setText(questionsMUSIQUE[questionChoisi][1]);
             reponse11.setBackgroundColor(Color.parseColor("#FF000000"));
@@ -102,102 +85,103 @@ public class quiz extends AppCompatActivity {
       //  };
 
 
-
+        /*Corriger ça*/
         reponse11.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //ChangerColeurs(texte du button; array)
+             @Override
+             public void onClick(View v) {
+                 //ChangerColeurs(texte du button; array)
+                 MontrerReponse(questionChoisi,1);
+             }
 
-                MontrerReponse;
-            }
         });
 
-
-         public static void MontrerReponse {
-             if (questionsMUSIQUE[questionChoisi][1] == questionsMUSIQUE[questionChoisi][5]) {
-                 reponse11.setBackgroundColor(Color.parseColor("#90EE90"));
-                 juste1++;
-             } else reponse11.setBackgroundColor(Color.parseColor("#FF0000"));
-
-             if (questionsMUSIQUE[questionChoisi][2] == questionsMUSIQUE[questionChoisi][5]) {
-                 reponse21.setBackgroundColor(Color.parseColor("#90EE90"));
-                 juste1++;
-             } else reponse21.setBackgroundColor(Color.parseColor("#FF0000"));
-
-             if (questionsMUSIQUE[questionChoisi][3] == questionsMUSIQUE[questionChoisi][5]) {
-                 reponse31.setBackgroundColor(Color.parseColor("#90EE90"));
-                 juste1++;
-             } else reponse31.setBackgroundColor(Color.parseColor("#FF0000"));
-
-             if (questionsMUSIQUE[questionChoisi][4] == questionsMUSIQUE[questionChoisi][5]) {
-                 reponse41.setBackgroundColor(Color.parseColor("#90EE90"));
-                 juste1++;
-             } else reponse41.setBackgroundColor(Color.parseColor("#FF0000"));
-
-             suite.setEnabled(true);
-             reponse11.setEnabled(false);
-             reponse21.setEnabled(false);
-             reponse31.setEnabled(false);
-             reponse41.setEnabled(false);
-
-             NbPoint1.setText("nombre de points : " + juste1);
-         }
-     }
-
-        reponse11.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {    int cu = 3;        }
-                });
-
-       /*
-       reponse21.setOnClickListener(new View.OnClickListener() {
+        reponse21.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //ChangerColeurs(texte du button; array)
-                reponse11.setBackgroundColor(Color.parseColor("#90EE90"));
-                reponse21.setBackgroundColor(Color.parseColor("#FF0000"));
-                reponse31.setBackgroundColor(Color.parseColor("#FF0000"));
-                reponse41.setBackgroundColor(Color.parseColor("#FF0000"));
-                suite.setEnabled(true);
-                reponse11.setEnabled(false);
-                reponse21.setEnabled(false);
-                reponse31.setEnabled(false);
-                reponse41.setEnabled(false);
+               MontrerReponse(questionChoisi,2);
             }
         });
 
         reponse31.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                reponse11.setBackgroundColor(Color.parseColor("#90EE90"));
-                reponse21.setBackgroundColor(Color.parseColor("#FF0000"));
-                reponse31.setBackgroundColor(Color.parseColor("#FF0000"));
-                reponse41.setBackgroundColor(Color.parseColor("#FF0000"));
-                suite.setEnabled(true);
-                reponse11.setEnabled(false);
-                reponse21.setEnabled(false);
-                reponse31.setEnabled(false);
-                reponse41.setEnabled(false);
+                MontrerReponse(questionChoisi,3);
             }
         });
 
         reponse41.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                reponse11.setBackgroundColor(Color.parseColor("#90EE90"));
-                reponse21.setBackgroundColor(Color.parseColor("#FF0000"));
-                reponse31.setBackgroundColor(Color.parseColor("#FF0000"));
-                reponse41.setBackgroundColor(Color.parseColor("#FF0000"));
-                suite.setEnabled(true);
-                reponse11.setEnabled(false);
-                reponse21.setEnabled(false);
-                reponse31.setEnabled(false);
-                reponse41.setEnabled(false);
+                MontrerReponse(questionChoisi,4);
             }
-        });*/
+        });
 
+        suite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                questionChoisi = RecreerQuestions();
+            }
+        });
+    }
+
+    private void MontrerReponse(int questionActuel, int reponse) {
+        //Changer coleurs
+        if (questionsMUSIQUE[questionActuel][1] == questionsMUSIQUE[questionActuel][5]) {
+            reponse11.setBackgroundColor(Color.parseColor("#90EE90"));
+        } else reponse11.setBackgroundColor(Color.parseColor("#FF0000"));
+
+        if (questionsMUSIQUE[questionActuel][2] == questionsMUSIQUE[questionActuel][5]) {
+            reponse21.setBackgroundColor(Color.parseColor("#90EE90"));
+        } else reponse21.setBackgroundColor(Color.parseColor("#FF0000"));
+
+        if (questionsMUSIQUE[questionActuel][3] == questionsMUSIQUE[questionActuel][5]) {
+            reponse31.setBackgroundColor(Color.parseColor("#90EE90"));
+        } else reponse31.setBackgroundColor(Color.parseColor("#FF0000"));
+
+        if (questionsMUSIQUE[questionActuel][4] == questionsMUSIQUE[questionActuel][5]) {
+            reponse41.setBackgroundColor(Color.parseColor("#90EE90"));
+        } else reponse41.setBackgroundColor(Color.parseColor("#FF0000"));
+
+        //Réponse correcte +1
+        if (questionsMUSIQUE[questionActuel][reponse] == questionsMUSIQUE[questionActuel][5])
+            juste1++;
+
+        suite.setEnabled(true);
+        reponse11.setEnabled(false);
+        reponse21.setEnabled(false);
+        reponse31.setEnabled(false);
+        reponse41.setEnabled(false);
+
+
+        NbPoint1.setText("nombre de points : " + juste1);
+    }
+
+    public int RecreerQuestions() {
+        int questionSuivante = 3; //Changer pour aleatoire apres et ajouter tableau historique
+            question11.setText(questionsMUSIQUE[questionSuivante][0]);
+            reponse11.setText(questionsMUSIQUE[questionSuivante][1]);
+            reponse11.setBackgroundColor(Color.parseColor("#FF000000"));
+            reponse21.setText(questionsMUSIQUE[questionSuivante][2]);
+            reponse21.setBackgroundColor(Color.parseColor("#FF000000"));
+            reponse31.setText(questionsMUSIQUE[questionSuivante][3]);
+            reponse31.setBackgroundColor(Color.parseColor("#FF000000"));
+            reponse41.setText(questionsMUSIQUE[questionSuivante][4]);
+            reponse41.setBackgroundColor(Color.parseColor("#FF000000"));
+
+            //Réactiver les buttons
+            reponse11.setEnabled(true);
+            reponse21.setEnabled(true);
+            reponse31.setEnabled(true);
+            reponse41.setEnabled(true);
+            suite.setEnabled(false);
+
+    juste1 = 0;
+
+            //Retourner la question suivante
+            return questionSuivante;
 
     }
+
 
 
 }
